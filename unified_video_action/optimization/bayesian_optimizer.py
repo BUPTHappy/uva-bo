66457140bfbaa41b8036b3342e487bd3943a9b6a
Zhuoer Liu <zhuoer@kth.se>
2026-01-23 21:15:18 +0100

    uva-with-bo

--- unified_video_action/optimization/bayesian_optimizer.py
+++ unified_video_action/optimization/bayesian_optimizer.py
@@ -30,7 +30,7 @@ class UVABayesianOptimizer:
             # Fewer sampling steps for speed
             num_sampling_steps_options = [1, 2, 3, 4, 5]
             temperature_range = (0.5, 1.0)  # Lower temperature for faster convergence
-            cfg_range = (0.5, 1.5)  # Narrower CFG range
+            cfg_range = (0.5, 1.0)  # Narrower CFG range
             
         elif self.optimization_mode == "performance_priority":
             # More sampling steps for performance
