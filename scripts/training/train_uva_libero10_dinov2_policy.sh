1a4bead772716b0789ffb7614de461f885f2c7fe
Zhuoer Liu <zhuoer@kth.se>
2026-06-16 01:56:28 +0800

    dimension align dinov2

--- scripts/training/train_uva_libero10_dinov2_policy.sh
+++ scripts/training/train_uva_libero10_dinov2_policy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Load video-stage weights from warm_start_checkpoint, train policy from epoch 0.
-accelerate launch --num_processes=3 train.py \
+accelerate launch --num_processes=6 train.py \
   --config-dir=unified_video_action/config \
   --config-name=uva_libero10_dinov2_policy.yaml \
   training.warm_start_checkpoint="checkpoints/libero10_video.ckpt" \
